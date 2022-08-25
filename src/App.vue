60ffebd9e1ec51bd3dde84bea0abde4bfc07cd25
IcarusAegis <46650308+IcarusAegis@users.noreply.github.com>
2022-08-25 10:14:16 +0800

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <nav>
-<!--      <router-link to="/">Home</router-link> |-->
-<!--      <router-link to="/about">About</router-link>-->
+      <router-link to="/blockinfo">blockinfo</router-link> |
+      <router-link to="/upload">upload</router-link>|
+      <router-link to="/setting">setting</router-link>
     </nav>
     <router-view/>
   </div>
